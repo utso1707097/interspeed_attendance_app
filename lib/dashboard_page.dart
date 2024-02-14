41e2867ef6f93ec881dac5d7993bf0bc5e6a6602
utso1707097 <utso097.csekuet@gmail.com>
2024-02-14 04:21:18 -0800

    Showing error message on wrong entry or exit

--- lib/dashboard_page.dart
+++ lib/dashboard_page.dart
@@ -23,7 +23,8 @@ class _DashboardPageState extends State<DashboardPage> {
   bool _isSignOutButtonClicked = false;
   double latitude = -1;
   double longitude = -1;
-  int accuracy = -100;
+  int accuracy = 100;
+  bool showProgressBar = false;
 
   getCurrentLocation() async {
     LocationPermission permission = await Geolocator.checkPermission();
@@ -58,8 +59,6 @@ class _DashboardPageState extends State<DashboardPage> {
   }
   @override
   void initState() {
-    _signInbase64Image = "";
-    _signOutbase64Image = "";
     super.initState();
   }
 
@@ -543,15 +542,28 @@ class _DashboardPageState extends State<DashboardPage> {
             GestureDetector(
               onTap: () {
                 // Handle button click here
+                setState(() {
+                  showProgressBar = true;
+                });
                 sendAttendanceData();
                 print('Button Clicked');
                 // Add your custom logic or navigation here
               },
-              child: Image.asset(
-                'assets/images/Submit tickxxxhdpi.png', // Replace with your asset image path
-                width: 70, // Adjust width as needed
-                height: 70, // Adjust height as needed
-                fit: BoxFit.cover, // Adjust the fit based on your requirement
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/Submit tickxxxhdpi.png',
+                    width: 70,
+                    height: 70,
+                    fit: BoxFit.cover,
+                  ),
+                  if (showProgressBar)
+                    const CircularProgressIndicator(
+                      strokeWidth: 2,
+                      valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+                    ),
+                ],
               ),
             )
           ],
@@ -701,18 +713,30 @@ class _DashboardPageState extends State<DashboardPage> {
     //Validate the request
     if (_isSignOutButtonClicked &&(_signOutbase64Image == null || _signOutbase64Image!.isEmpty)) {
       // Handle the case when _base64Image is empty
+      setState(() {
+        showProgressBar = false;
+      });
       print('Error: Image data is empty');
+      _showAttendacneDialog("Failed", "Required selfie! please click a selfie", 0);
       return;
     }
 
     if (_isSignInButtonClicked &&(_signInbase64Image == null || _signInbase64Image!.isEmpty)) {
       // Handle the case when _base64Image is empty
+      setState(() {
+        showProgressBar = false;
+      });
       print('Error: Image data is empty');
+      _showAttendacneDialog("Failed", "Required selfie! please click a selfie", 0);
       return;
     }
 
     if (latitude == -1 || longitude == -1) {
       // Handle the case when latitude or longitude is not set
+      setState(() {
+        showProgressBar = false;
+      });
+      _showAttendacneDialog("Failed", "Location is mandatory for this action! Make sure location service is enabled", 0);
       print('Error: Latitude or longitude not set');
       return;
     }
@@ -735,15 +759,72 @@ class _DashboardPageState extends State<DashboardPage> {
       if (response.statusCode == 200) {
         // Request was successful
         Map<String, dynamic> responseData = jsonDecode(response.body);
+        if(responseData['success'] && _isSignInButtonClicked)_showAttendacneDialog("Success", "You have successfully entered!", 200);
+        if(responseData['success'] && _isSignOutButtonClicked)_showAttendacneDialog("Success", "Bye bye, see you next time!", 200);
+        else _showAttendacneDialog("Failed", "Attendance already recorded!", 0);
+        setState(() {
+          _isSignInButtonClicked?_signInbase64Image= "":_signOutbase64Image="";
+        });
         // Process the response data
         print(responseData);
       } else {
         // Request failed
+        _showAttendacneDialog("Failed", "Check your internet connection or login again!", 0);
         print('Request failed with status: ${response.statusCode}');
       }
     } catch (error) {
       // Handle errors
+      _showAttendacneDialog("Failed", "Check your internet connection", 0);
       print('Error sending request: $error');
     }
+    finally{
+      setState(() {
+        showProgressBar = false;
+      });
+    }
+  }
+
+  void _showAttendacneDialog(String title,String message,int statusCode) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: const Color(0xFF333333), // Body color
+          title: Container(
+
+            // color: const Color(0xFF1a1a1a), // Header area color
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(color: Colors.white),
+                ),
+                Icon(
+                  statusCode == 200?Icons.check :Icons.error,
+                  color: statusCode == 200? Colors.green : Colors.red,
+                ),
+              ],
+            ),
+          ),
+          content: Text(
+            message,
+            style: const TextStyle(color: Colors.white),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('OK', style: TextStyle(color: Colors.white)),
+            ),
+          ],
+          contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0), // Adjust the radius as needed
+          ),
+        );
+      },
+    );
   }
 }
